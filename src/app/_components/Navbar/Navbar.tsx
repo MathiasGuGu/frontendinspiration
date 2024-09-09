786c395379f49aa0b9b35ca6b86054228b9cc4cb
mathiasgugu <matgum51873@stud.noroff.no>
2024-09-10 00:42:14 +0200

    lcp and ssr impl

--- src/app/_components/Navbar/Navbar.tsx
+++ src/app/_components/Navbar/Navbar.tsx
@@ -2,14 +2,7 @@
 import { useIsMobile } from "@/hooks/useIsMobile";
 import NavbarMobile from "./NavbarMobile";
 import NavbarDesktop from "./NavbarDesktop";
-import Marquee from "react-fast-marquee";
-import {
-  LucideSmile,
-  Smile,
-  SmileIcon,
-  Star,
-  WandSparkles,
-} from "lucide-react";
+
 const Navbar = () => {
   const isMobile = useIsMobile();
 
@@ -17,16 +10,6 @@ const Navbar = () => {
 
   return (
     <nav className="flex h-auto min-h-16 w-screen flex-col items-center justify-center md:p-0">
-      {/* <div className="relative mb-4 flex h-10 w-full items-center justify-center bg-purple-400 text-purple-900">
-        <Marquee gradient={false} speed={15} direction="left">
-          <div className="flex items-center space-x-32">
-            <p>MORE RESOURCES ARE COMING </p>
-            <p>MORE RESOURCES ARE COMING </p>
-            <p>MORE RESOURCES ARE COMING </p>
-            <p>MORE RESOURCES ARE COMING </p>
-          </div>
-        </Marquee>
-      </div> */}
       <div className="flex h-full w-full items-center justify-between md:max-w-[90%]">
         {isMobile ? <NavbarMobile /> : <NavbarDesktop />}
       </div>
